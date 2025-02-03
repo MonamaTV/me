c1f2f7d1c9ff71d8c48c7b5d70b1f8c08acba89f
Tadima <keletsovincent92@gmail.com>
2025-02-03 15:15:17 +0200

    fixing bugs

--- app/main.js
+++ app/main.js
@@ -4,9 +4,6 @@ const getCurrentWeather = async (lat, lon) => {
   const response = await fetch(`https://api.openweathermap.org/data/2.5/weather?lat=${lat}&lon=${lon}&units=metric&appid=dcaa0fda1a8ccd9fbbea884e9e3e3245`);
   const data = await response.json();
 
-  const res = await fetch("https://www.latlong.net/c/?lat=-26.144689&long=28.042164");
-
-  
 
 
   return {...data?.main, name: data.name };
